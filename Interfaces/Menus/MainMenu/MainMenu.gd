da7210460b30e0eea0c31a25914f7f097e1f9ff7
Okeeee-Holz <150455091+Okeeee-Holz@users.noreply.github.com>
2025-08-07 00:21:33 +0200

    Options tabs

--- Interfaces/Menus/MainMenu/MainMenu.gd
+++ Interfaces/Menus/MainMenu/MainMenu.gd
@@ -6,3 +6,20 @@ func _ready() -> void:
 	for child in get_children():
 		child.hide()
 		$StartMenu.show()
+		
+	# Connect buttons
+	connectHoverAndFocus(get_children())
+	child_entered_tree.connect(onChildAdded)
+
+func onChildAdded(node: Node):
+	if node is Button:
+		node.mouse_entered.connect(onButtonHover.bind(node))
+
+func connectHoverAndFocus(nodes: Array):
+	for node in nodes:
+		if node is Button:
+			node.mouse_entered.connect(onButtonHover.bind(node))
+		connectHoverAndFocus(node.get_children())
+
+func onButtonHover(button: Button):
+	button.grab_focus()
