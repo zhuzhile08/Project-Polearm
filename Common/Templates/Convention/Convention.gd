42bd0a42ae18a12985bd19866c2146ad237fc8e0
Zhile Zhu <zhuzhile08@gmail.com>
2026-02-22 19:08:29 +0100

    Add a file to document conventions

---
+++ Common/Templates/Convention/Convention.gd
@@ -0,0 +1,74 @@
+extends Node
+# For nodes with extreme custom functionality, assign them a class name and add the class 
+# to the scene tree instead of attaching a script. This also supports type safety
+class_name Convention
+
+
+# 1. Always leave two spaces between the top of the file and the first reagion
+# 2. Always leave two sapces between two regions
+# 3. There should be no place in the file with more than two spaces
+# 4. Always leave a space when not commenting a single thing, but a whole group of things
+# 5. Never comment regions
+# 6. Region order may not be changed and MUST follow below order
+
+#region Enums
+
+
+
+#endregion
+
+
+#region Helper classes
+
+
+
+#endregion
+
+
+#region Exported variables
+
+
+
+#endregion
+
+
+#region Scene members
+
+
+
+#endregion
+
+
+#region Member variables
+
+
+
+#endregion
+
+
+#region Built-in funcitons
+
+
+
+#endregion
+
+
+#region Public funcitons
+
+
+
+#endregion
+
+
+#region Private functions
+
+
+
+#endregion
+
+
+#region Implementation functions
+
+
+
+#endregion
